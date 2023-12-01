3536f9fc1eba8be09d354d117afa491e8aa50337
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-12-01 10:45:56 +0100

    - utils

--- scatch/utils.py
+++ scatch/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import product
 
 def gauge_transform_xor(M, G):
     """
@@ -21,3 +22,21 @@ def gauge_transform_xor(M, G):
             result[i, j] = temp_result
 
     return result
+
+
+def generate_binary_matrices(shape):
+    possibilities = list(product([0, 1], repeat=shape[0]*shape[1]))
+    matrices = [np.array(matrix).reshape(shape) for matrix in possibilities]
+    return matrices
+
+def filter_invertible_matrices(matrices):
+    invertible_matrices = [matrix for matrix in matrices if np.linalg.det(matrix) != 0]
+    return invertible_matrices
+
+def generate_invertible(rank):
+    """
+    Generate all invertible matrices
+    :param rank: side length of the square matrix
+    :return:
+    """
+    return filter_invertible_matrices(generate_binary_matrices((rank,rank)))
