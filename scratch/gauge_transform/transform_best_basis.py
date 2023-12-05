ce5ef9b9c510284541f3e541e4711a37d350cfe7
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-12-05 11:15:00 +0100

    docstrings

--- scratch/gauge_transform/transform_best_basis.py
+++ scratch/gauge_transform/transform_best_basis.py
@@ -29,20 +29,36 @@ class best_basis():
         :param m: _description_
         :type m: _type_
         """
-        __score_bases(m)
-        self.best_g_score = np.min(self.scores)
+        self.__score_bases(m)
+        self.best_g_score = np.min(list(self.scores.keys()))
         self.all_best = self.scores[self.best_g_score]
         self.best_g = self.all_best[0]
 
     def __score_bases(self, m) -> None:
+        """
+        Generate and score alternative bases.
+        Do GM = M' for all g in {g}.
+
+        :param m: square binary matrix
+        :type m: _type_
+        """
+        assert set(m) <= {0,1}, "values outside 0, 1"
+        assert m.shape[0] == m.shape[1], "m is not square"
+        assert len(m.shape) == 2, "m is not 2d"
+        
         t = generate_invertible(len(m))
-        t2 = np.empty(shape(len(t), size, size))
+        t2 = np.empty(shape = (len(t), size, size))
 
         for i, g_i in enumerate(t):
             m2 = np.array(gauge_transform_xor(m, g_i))
             t2[i,:,:] = m2
 
-            self.scores[np.sum(m2)].append(m2) # FIXME: Note: Placeholder scoring function.
+            score  = self.__scoring_sum(m2)
+            self.scores[score].append(m2) # FIXME: Note: Placeholder scoring function.
+
+    @staticmethod
+    def __scoring_sum(m2) -> float:
+        return np.sum(m2)
        
         
 
@@ -51,26 +67,10 @@ if __name__ == "__main__":
     size = 3
     # m = np.random.randint(0,2,(3,3),dtype=int)
     m = np.identity(size,dtype=int)
-    print(m)
-    t = generate_invertible(len(m))
-    t2 = np.empty(shape=(len(t),size,size))
-    scores = defaultdict(list)
-    for i, g_i in enumerate(t):
-        m2 = np.array(gauge_transform_xor(m,g_i))
-        t2[i,:,:] = m2
-        print(m2, "\n")
-        scores[np.sum(m2)].append(m2)
-
-
-        # score M'
-        # score_m2 = np.sum(m2)
-    # print(np.sum(np.array(t2),axis=0))
-
-    # apply all gauge transforms
-    # M: row= spin variable, column = IM/spin operator
-    print(scores.keys())
-    print("3")
-    [print(i,"\n") for i in scores[3]]
+    B = best_basis(m)
+    B.find_best()
+    
+    [print(i,"\n") for i in B.all_best]
     # FIXME: these are all the same matrix that can be reached with elementary row and column operations,
     #  maybe instead of checking if something is invertible, we can generate matrices that are only different in the nr of elements
     #  and then filter those before constructing all same ones with elementary row operations
