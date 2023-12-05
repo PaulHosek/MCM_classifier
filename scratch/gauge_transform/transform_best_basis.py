a43332071107e7333344579579f9f5f168763034
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2023-12-05 11:55:13 +0100

    added generation code to best basis class

--- scratch/gauge_transform/transform_best_basis.py
+++ scratch/gauge_transform/transform_best_basis.py
@@ -1,14 +1,10 @@
 import numpy as np
 from collections import defaultdict
 
+from itertools import product
 from transform.gauge_transform_algs import gauge_transform_xor
 from inversion.invertible_matrices_algs import generate_invertible
 
-
-
-# build all possible gauge transforms for this size
-# apply all gauge transforms
-# find the one that is
 
 class Best_basis():
     def __init__(self, m) -> None:
@@ -30,9 +26,10 @@ class Best_basis():
         :type m: _type_
         """
         self.__score_bases(m)
-        self.best_g_score = np.min(list(self.scores.keys()))
+        self.best_m_score = np.min(list(self.scores.keys()))
         self.all_best = self.scores[self.best_g_score]
-        self.best_g = self.all_best[0]
+        self.best_m = self.all_best[0]
+
 
     def __score_bases(self, m) -> None:
         """
@@ -46,11 +43,11 @@ class Best_basis():
         assert m.shape[0] == m.shape[1], "m is not square"
         assert len(m.shape) == 2, "m is not 2d"
 
-        t = generate_invertible(len(m))
+        t = self.__generate_invertible(len(m))
         t2 = np.empty(shape = (len(t), size, size))
 
         for i, g_i in enumerate(t):
-            m2 = np.array(gauge_transform_xor(m, g_i))
+            m2 = np.array(self.__gauge_transform_xor(m, g_i))
             t2[i,:,:] = m2
 
             score  = self.__scoring_sum(m2)
@@ -60,7 +57,50 @@ class Best_basis():
     def __scoring_sum(m2) -> float:
         return np.sum(m2)
        
-        
+    @staticmethod    
+    def __gauge_transform_xor(m, g):
+        """
+        Compute the XOR matrix product of two binary matrices.
+
+        Parameters:
+        - M: Binary matrix
+        - G: Binary matrix
+
+        Returns:
+        - XOR matrix product of M and G
+        """
+        result = np.zeros((m.shape[0], g.shape[1]), dtype=int)
+
+        for i in range(m.shape[0]):
+            for j in range(g.shape[1]):
+                temp_result = 0
+                for k in range(m.shape[1]):
+                    temp_result ^= m[i, k] & g[k, j]
+                result[i, j] = temp_result
+
+        return result
+
+    @staticmethod
+    def __generate_binary_matrices(shape):
+        possibilities = list(product([0, 1], repeat=shape[0]*shape[1]))
+        matrices = [np.array(matrix).reshape(shape) for matrix in possibilities]
+        return matrices
+    
+    @staticmethod
+    def __filter_invertible_matrices(matrices):
+        invertible_matrices = [matrix for matrix in matrices if np.linalg.det(matrix) != 0]
+        return invertible_matrices
+    
+    @staticmethod
+    def __generate_invertible(rank):
+        """
+        Generate all invertible matrices
+        :param rank: side length of the square matrix
+        :return:
+        """
+        return filter_invertible_matrices(generate_binary_matrices((rank,rank)))
+
+
 
 
 if __name__ == "__main__":
@@ -69,7 +109,7 @@ if __name__ == "__main__":
     m = np.identity(size,dtype=int)
     B = Best_basis(m)
     B.find_best()
-    print(B)
+    print(B.scores)
     
     [print(i,"\n") for i in B.all_best]
     # FIXME: these are all the same matrix that can be reached with elementary row and column operations,
