4f06baf60d3b125d87e2a966967666fc9474aa0a
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-01-08 16:48:32 +0100

    small mcm example classes done

--- scratch/0_prob/small_mcm_clf.py
+++ scratch/0_prob/small_mcm_clf.py
@@ -1,3 +1,6 @@
+from locale import DAY_6
+import matplotlib.pyplot as plt
+import array
 from itertools import product
 from re import I
 import numpy as np
@@ -7,40 +10,51 @@ import numpy as np
 # we want to know what happens what we can still say about the shape we observe if we e.g., cut out a pixel that is 0 in the test data
 
 
-def generate_class_dataset(n,cls):
+def generate_class_dataset(cls):
     """Generate fake data for the T class for  a square grid of size n
     - classes equivalent in nr of modeled spins and nr 0s 
     Top-class
-    [a,b,e]
-    [c,d,f]
+    [a,b,c]
+    [d,e,f]
     [0,0,0]
 
     Corner_class
     [0,a,b]
-    [c,0,e]
-    [f,g,0]
+    [c,0,d]
+    [e,f,0]
 
     :param n: sidelength. 3 or 4
     :param cls: class. t or c
     """
 
-    assert n in [3,4], "n must be 3 or 4"
-    assert cls.lower() in ["t","c"]
 
-    if n == 3:
-        if cls.lower() == "t":
+    if cls.lower() == "t":
 
-            upper = __generate_binary_matrices((2,3))
-            matrices = [np.vstack((i,np.array([0,0,0]))) for i in res]
-            return matrices
-        
-        elif cls.lower() == "c":
-            upper = __generate_binary_matrices((2,3))
-            matrices = [np.vstack((i,np.array([0,0,0]))) for i in res]
-            # transformed_arrays = [np.eye(n, dtype=int)[:-1, :] arr for arr in matrices]
+        upper = __generate_binary_matrices((2,3))
+        matrices = [np.vstack((i,np.array([0,0,0]))) for i in upper]
+        return matrices
+    
+    elif cls.lower() == "c":
+        upper = __generate_binary_matrices((2,3))
+        matrices = [np.vstack((i,np.array([0,0,0]))) for i in upper]
+        return [top_to_corner(m) for m in matrices]
+        # transformed_arrays = [np.eye(n, dtype=int)[:-1, :] arr for arr in matrices]
+    else:
+        raise ValueError("Class must be either t or c")
 
 
+def top_to_corner(top_matrix):
+    corner_matrix = np.zeros(top_matrix.shape)
+    corner_matrix[0, 1] = top_matrix[0, 0]
+    corner_matrix[0, 2] = top_matrix[0, 1]
 
+    corner_matrix[1, 0] = top_matrix[0, 2]
+    corner_matrix[1, 2] = top_matrix[1, 0]
+
+    corner_matrix[2, 0] = top_matrix[1, 1]
+    corner_matrix[2, 1] = top_matrix[1, 2]
+
+    return corner_matrix
 
 
 def __generate_binary_matrices(shape: tuple[int, int]):
@@ -52,21 +66,20 @@ def __generate_binary_matrices(shape: tuple[int, int]):
 def bottom_to_diag():
     pass
 
-if __name__ == "__main__":
-    res = __generate_binary_matrices((2,3))
-    matrices = [np.vstack((i,np.array([0,0,0]))) for i in res]
-    # transformed_arrays = [np.eye(arr.shape[0], dtype=int)[:-1, :] for arr in matrices]
-    # transformed_arrays = np.eye(3,dtype=int)
-    m = np.array(matrices)
-    # f2 = m[-2,:,:][None, ...]
-    f2 = m[-2:,:,:]
-    print(f2)
-    print()
-    print(f2.swapaxes(0,1))
-    
+def arr_to_int(binary_arr):
+    return int(binary_arr.dot(1 << np.arange(binary_arr.shape[-1] - 1, -1, -1)))
 
-    # res = np.vstack(res[],np.array([0,0,0],axis=2))
-    # print(res)
+
+    
+if __name__ == "__main__":
+    m = generate_class_dataset("c")
+    # [print(i,"\n") for i in m]
+    # print(np.sum(m,axis=0))a
+    data_c = [arr_to_int(i.flatten()) for i in generate_class_dataset("c")]
+    data_t = [arr_to_int(i.flatten()) for i in generate_class_dataset("t")]
+
+
+
 
             
 
