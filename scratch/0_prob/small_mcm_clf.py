4a399f1c8ab1bb2af84a63d19f724a8dc0db29e5
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-01-11 15:12:24 +0100

    loaded data correcly

--- scratch/0_prob/small_mcm_clf.py
+++ scratch/0_prob/small_mcm_clf.py
@@ -1,11 +1,13 @@
-from locale import DAY_6
-from tkinter import N
 import matplotlib.pyplot as plt
 import array
 from itertools import product
-from re import I
 import numpy as np
-# from repositories.bsc.src.classify import MCM_Classifier
+import sys
+from src.loaders import load_data, load_labels
+from src.classify import MCM_Classifier
+from src.plot import plot_confusion_matrix, plot_label_prob_diff
+
+
 import small_mcm_tools as tools
 import ebo_best_basis
 import numpy as np
@@ -20,12 +22,22 @@ import numpy as np
 
 n_categories = 2
 n_variables = 9
-def main():
-    classifier = MCM_Classifier(
-        n_categories, n_variables, "placeholder", "placeholder")
+mcm_filename_format = "train-images-unlabeled-{}_bootstrap_comms.dat"
+data_filename_format = "train-images-unlabeled-{}_bootstrap.dat"
 
-    train_data = None
-    train_labels = None
+def main():
+    print("{:-^50}".format("  MCM-Classifier  ")) 
+
+    test_data = load_data("scratch/0_prob/INPUT/data/test-images-unlabeled-all-uniform.txt").astype(int)
+    test_labels = load_labels("scratch/0_prob/INPUT/data/test-labels-uniform.txt").astype(int)
+    # Step 1: Initialize classifier
+    print(test_data)
+    classifier = MCM_Classifier(
+    n_categories, n_variables, mcm_filename_format, data_filename_format
+    )
+
+    # train_data = None
+    # train_labels = None
 
 # find best MCM
 
@@ -35,6 +47,42 @@ def main():
     
 if __name__ == "__main__":
     m = tools.generate_class_dataset("c")
+
+
+    # Generate training and test datasets
+    c_binary_data = tools.generate_class_dataset("c")
+    t_binary_data = tools.generate_class_dataset("t")
+    np.savetxt("scratch/0_prob/INPUT/data/train-images-unlabeled-0.dat", np.concatenate(c_binary_data[:60]), delimiter='', fmt='%i',)
+    np.savetxt('scratch/0_prob/INPUT/data/train-images-unlabeled-1.dat', np.concatenate(t_binary_data[:60]), delimiter='', fmt='%i',)
+    
+    test_c_d = c_binary_data[50:]
+    test_t_d = t_binary_data[50:]
+    test_c_labels = np.zeros(len(test_c_d))
+    test_labels = np.concatenate((np.ones(len(test_c_d),dtype=int), np.zeros(len(test_t_d),dtype=int)))
+    test_data = np.concatenate([*test_c_d,*test_t_d])
+    np.savetxt('scratch/0_prob/INPUT/data/test-images-unlabeled-all-uniform.txt', test_data, delimiter='', fmt='%i',)
+    np.savetxt('scratch/0_prob/INPUT/data/test-labels-uniform.txt', test_labels, delimiter='', fmt='%i',)
+   
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # [print(i,"\n") for i in m]
     # print(np.sum(m,axis=0))a
 
@@ -42,33 +90,6 @@ if __name__ == "__main__":
     # data_t = [tools.arr_to_int(i.flatten()) for i in tools.generate_class_dataset("t")]
     # basis_c = ebo_best_basis.find_best_basis(9,data_c)
     # basis_t = ebo_best_basis.find_best_basis(9,data_t)
-
-    c_binary_data = tools.generate_class_dataset("c")
-    t_binary_data = tools.generate_class_dataset("t")
-    train_c_d = c_binary_data[:60]
-    test_c_d = c_binary_data[50:]# some overlap so we can see how the labels are affected by smoothing
-
-    np.savetxt('INPUT/data/train_c_data.csv', np.vstack(c_binary_data[:60]), delimiter=',')
-    np.savetxt('INPUT/data/train_t_data.csv', np.vstack(t_binary_data[:60]), delimiter=',')
-    
-
-
-    mcm_c = None
-    mcm_t = None
-
-    # Save test data
-    test_c_d = c_binary_data[50:]
-    test_t_d = t_binary_data[50:]
-
-    np.savetxt('INPUT/data/test_data.csv', np.vstack((test_c_d, test_t_d)), delimiter=',')
-
-    # Save labels
-    test_c_labels = np.zeros(len(test_c_d))
-    test_t_labels = np.ones(len(test_t_d))
-
-    np.savetxt('INPUT/data/test_labels.csv', np.concatenate((test_c_labels, test_t_labels)), delimiter=',')
-
-
 
 
             
