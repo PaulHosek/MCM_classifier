a4a227287c5d07cc748548a05c6ea8efa13faee8
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-02-06 15:09:17 +0100

    Remove commented code and unused imports

--- Classifier_1/classifier/src/helpers.py
+++ Classifier_1/classifier/src/helpers.py
@@ -31,7 +31,26 @@ def generate_bootstrap_samples(data_path: str,filename:str, bootstrap_name: str,
 
 
 
+# def nudge_dataset(X, Y):
+#     """
+#     This produces a dataset 5 times bigger than the original one,
+#     by moving the 8x8 images in X around by 1px to left, right, down, up
+#     """
+#     direction_vectors = [
+#         [[0, 1, 0], [0, 0, 0], [0, 0, 0]],
+#         [[0, 0, 0], [1, 0, 0], [0, 0, 0]],
+#         [[0, 0, 0], [0, 0, 1], [0, 0, 0]],
+#         [[0, 0, 0], [0, 0, 0], [0, 1, 0]],
+#     ]
 
+#     def shift(x, w):
+#         return convolve(x.reshape((8, 8)), mode="constant", weights=w).ravel()
+
+#     X = np.concatenate(
+#         [X] + [np.apply_along_axis(shift, 1, X, vector) for vector in direction_vectors]
+#     )
+#     Y = np.concatenate([Y for _ in range(5)], axis=0)
+#     return X, Y
 
 
 
