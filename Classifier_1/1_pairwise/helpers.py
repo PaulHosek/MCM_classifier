dd8c9212d511d49bd9f0dea6f3291c1f6eb02b63
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-03-12 17:03:57 +0100

    subsample data ace works

--- Classifier_1/1_pairwise/helpers.py
+++ Classifier_1/1_pairwise/helpers.py
@@ -4,28 +4,40 @@ import numpy as np
 
 
 
-
-
-
-
-
-
-
-def subsample_data(sample_size, all_data_path="../INPUT_all/data", input_data_path="../INPUT/data", seed=42,fname_start = "train-"):
+def subsample_data_ace(sample_size, all_data_path="../INPUT_all/data", input_data_path="../INPUT/data", seed=42,fname_start = "train-"):
     """Clear the input_data_path folder and fill it with samples from the all_data_path folder.
+        Note: Every sample will be getting its own subfolder in this "ace" version.
     
     :param sample_size: if None then take whole sample, otherwise provide integer of how many samples. Must be <= available samples.
 
     """
     rng = np.random.default_rng(seed)
 
-    # Iterate over the files and delete the ones that start with "train-"
-    for file in os.listdir(input_data_path):
-        if file.startswith(fname_start):
-            os.remove(os.path.join(input_data_path, file))
+    clear_directory(input_data_path)
 
     # generate new input data 
     for file in os.listdir(all_data_path):
         if file.startswith(fname_start):
             inp = np.loadtxt(os.path.join(all_data_path,file), dtype="str")
-            np.savetxt(os.path.join(input_data_path, file), rng.choice(inp, sample_size,replace=False), fmt="%s")
+            subfolder_name = file.split(".")[0]  # extract the file name without extension
+            subfolder_path = os.path.join(input_data_path, subfolder_name)  # create subfolder path
+
+            os.makedirs(subfolder_path, exist_ok=True)  # create subfolder if it doesn't exist
+            np.savetxt(os.path.join(subfolder_path, file), rng.choice(inp, sample_size, replace=False), fmt="%s")
+
+
+def clear_directory(path):
+    """Deletes all files and subdirectories in a given directory.
+
+    :param path: Path to the directory to clear.
+    """
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+
--- Classifier_1/1_pairwise/pairwise_classify.py
+++ Classifier_1/1_pairwise/pairwise_classify.py
@@ -5,6 +5,7 @@ import utils.ACEtools as ACEtools
 import helpers
 import os
 import subprocess
+import shutil
 
 
 # want class that only has 1 digit 
@@ -27,6 +28,16 @@ def convert_to_spaced(INPUT_dir = "INPUT/data"):
 
 # call ACEtool code on it to generate .p file
 def call_ace(ace_args: tuple):
+    """Call Adaptive cluster expansion (ACE) code.
+    Note, this is a sequential implementation. Only one pairwise model is fitted at the time.
+
+    :param ace_args: tuple of what string elements that make up the command line argument
+    :type ace_args: tuple
+    :raises KeyboardInterrupt: Raised when ACE is interrupted
+    :raises SystemExit: Raised when ACE process exits unexpectedly
+    :return: The subprocess.Popen object representing the ACE process
+    :rtype: subprocess.Popen
+    """
     with open(os.devnull, "w") as f:
         try:
             p = subprocess.Popen(ace_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -40,13 +51,15 @@ def call_ace(ace_args: tuple):
     return p
 
 
-
-
 # test that there were no variables deleted -> check that specific line (should never happen since we always add the all 0s and all 1s but better have safeguard)
+
+
+
 # call ACE fitting on it with right flags
 # call MC learning QLS algo on it
 
 # at end should have 10 .p files -> one for each category
 
-
+if __name__ == "__main__":
+)
 
