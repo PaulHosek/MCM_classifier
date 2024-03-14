2e0ab8e691c9c67d573e231bbec3b3f99ee7be3e
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-03-14 10:58:30 +0100

    subsample now only reinitializes single category folder

--- Classifier_1/1_pairwise/helpers.py
+++ Classifier_1/1_pairwise/helpers.py
@@ -4,20 +4,19 @@ import shutil
 
 
 
-def subsample_data_ace(sample_size, all_data_path="../INPUT_all/data/", input_data_path="../INPUT/data/", seed=42,fname_start = "train-"):
-    """Clear the input_data_path folder and fill it with samples from the all_data_path folder.
-        Note: Every sample will be getting its own subfolder in this "ace" version.
+def subsample_cat_ace(sample_size,fname, all_data_path="./INPUT_all/data/", input_data_path="./INPUT/data/", seed=42):
+    """Clear input_data_path and fill it, from the all_data_path, with a single folder of the category we are interested in.
     
     :param sample_size: if None then take whole sample, otherwise provide integer of how many samples. Must be <= available samples.
 
     """
     rng = np.random.default_rng(seed)
 
-    clear_directory(input_data_path)
-
+    clear_cat(input_data_path, fname)
     # generate new input data 
     for file in os.listdir(all_data_path):
-        if file.startswith(fname_start):
+        if file.startswith(fname):
+            print(file)
             inp = np.loadtxt(os.path.join(all_data_path,file), dtype="str")
             subfolder_name = file.split(".")[0]  
             subfolder_path = os.path.join(input_data_path, subfolder_name) 
@@ -27,19 +26,29 @@ def subsample_data_ace(sample_size, all_data_path="../INPUT_all/data/", input_da
             arr = np.append(arr, ["0"*121, "1"*121])
             np.savetxt(os.path.join(subfolder_path, file), arr, fmt="%s")
 
+def clear_cat(path, folder_prefix):
+    """In path, delete all folders that begin with folder_prefix."""
+    for folder in os.listdir(path):
+        if folder.startswith(folder_prefix):
+            folder_path = os.path.join(path, folder)
+            shutil.rmtree(folder_path)
+    
+if __name__ == "__main__": 
+    subsample_cat_ace(10,"train-images-unlabeled-0")
+    
+    
+# def clear_directory(path):
+#     """Deletes all files and subdirectories in a given directory.
 
-def clear_directory(path):
-    """Deletes all files and subdirectories in a given directory.
-
-    :param path: Path to the directory to clear.
-    """
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
+#     :param path: Path to the directory to clear.
+#     """
+#     ford filename in os.listdir(path):
+#         file_path = os.path.join(path, filename)
+#         try:
+#             if os.path.isfile(file_path) or os.path.islink(file_path):
+#                 os.unlink(file_path)
+#             elif os.path.isdir(file_path):
+#                 shutil.rmtree(file_path)
+#         except Exception as e:
+#             print(f'Failed to delete {file_path}. Reason: {e}')
 
--- Classifier_1/1_pairwise/pairwise_classify.py
+++ Classifier_1/1_pairwise/pairwise_classify.py
@@ -13,81 +13,130 @@ import shutil
 # need
 # for each class
 
+class Pairwise_model():
+    # fit single pairwise model/ single category
+    def __init__(self, sample_size, INPUT_dir, fname) -> None:
+        self.sample_size = sample_size
+        self.INPUT_dir = INPUT_dir
+        self.fname = fname
+        self.cat_dir = os.path.join(INPUT_dir,fname)
 
-# convert data to spaced format #NOTE UNTESTED!
-def convert_to_spaced(INPUT_dir = "INPUT/data"):
-    """Converts all files in the INPUT folder and its subfolders into the spaced format."""
-    for root, dirs, files in os.walk(INPUT_dir):
-        for filename in files:
-            if filename.endswith(".dat"):
-                path = os.path.join(root, filename)
-                file = np.genfromtxt(path, dtype=int, delimiter=1)
-                np.savetxt(path[:-4] + "_sep" + ".dat", file, fmt="%d", delimiter=" ")
+    def fit():
+    # make this a class and one main function to do the steps in order
+                            # subsample, convert, ACEtools, nodeletions
+         helpers.subsample_data_ace(10, all_data_path="./INPUT_all/data", input_data_path="./INPUT/data")
+        convert_to_spaced()
+
+    # path = "INPUT/data/train-images-unlabeled-0/train-images-unlabeled-0_sep.dat"
+    # res = ACEtools.WriteCMSA("binary",path)
+    # no_deletions()
+
+    pass
+
+
+# test that there were no variables deleted -> check that specific line (should never happen since we always add the all 0s and all 1s but better have safeguard)
+    def no_deletions(self):
+        """Check after running ACEtools, if there were variables deleted because they were always 0.
+        This should not happen as it messes up the order later. The subsampler should avoid that this happens.
+        This is a function to perform this sanity check."""
+        for root, dirs, files in os.walk(self.cat_dir):
+            for filename in files:
+                if filename.endswith(".rep"):
+                    path = os.path.join(root, filename)
+                    with open(path, 'r') as file:
+                        lines = file.readlines()
+                        if len(lines) >= 8 and lines[7].strip() != "":
+                            raise ValueError("Variables were deleted: \n" + lines[7].strip())
+
+
+
+    def convert_to_spaced(self):
+        """Converts all files in the current CATEGORY's folder and its subfolders from binary strings to binary integers with spaces:
+        e.g., 000111 -> 0 0 0 1 1 1.
+        """
+        for root, dirs, files in os.walk(self.cat_dir):
+            for filename in files:
+                if filename.endswith(".dat"):
+                    path = os.path.join(root, filename)
+                    file = np.genfromtxt(path, dtype=int, delimiter=1)
+                    np.savetxt(path[:-4] + "_sep" + ".dat", file, fmt="%d", delimiter=" ")
+
+
 
 
 
 # call ACEtool code on it to generate .p file
-def call_ace(ace_args: tuple):
-    """Call Adaptive cluster expansion (ACE) code.
-    Note, this is a sequential implementation. Only one pairwise model is fitted at the time.
+    def call_exec(args: tuple):
+        """Call executable.
+        Note, this is a sequential implementation. Only one pairwise model is fitted at the time.
 
-    :param ace_args: tuple of what string elements that make up the command line argument
-    :type ace_args: tuple
-    :raises KeyboardInterrupt: Raised when ACE is interrupted
-    :raises SystemExit: Raised when ACE process exits unexpectedly
-    :return: The subprocess.Popen object representing the ACE process
-    :rtype: subprocess.Popen
-    """
-    with open(os.devnull, "w") as f:
-        try:
-            p = subprocess.Popen(ace_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        except KeyboardInterrupt:
-            raise KeyboardInterrupt("ACE interrupted")
-        except SystemExit:
-            raise SystemExit("ACE process exited unexpectedly")
-        stat = p.wait
-    if stat == 0:
-        print(f"\N{check mark} ACE done.")
-    return p
+        :param args: tuple of what string elements that make up the command line argument
+        :type args: tuple
+        :raises KeyboardInterrupt: Raised when ACE is interrupted
+        :raises SystemExit: Raised when ACE process exits unexpectedly
+        :return: The subprocess.Popen object representing the ACE process
+        :rtype: subprocess.Popen
+        """
+        with open(os.devnull, "w") as f:
+            try:
+                p = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt("Process interrupted")
+            except SystemExit:
+                raise SystemExit("Process process exited unexpectedly")
+            stat = p.wait
+        if stat == 0:
+            print(f"\N{check mark} Process done.")
+        return p
 
 
-def build_args(path_to_exe:str, p_dir:str ,p_fname:str , sample_size:int, auto_l2=True, *args):
-    """Generate the arguments for the ACE algorithm.
 
-    :param path_to_exe: path to the executable file. e.g., ./bin/ace
-    :type path_to_exe: str
-    :param p_dir: directory of the .p file to do the ace on. e.g., ./INPUT/data/img1
-    :type p_dir: str
-    :param p_fname: filename of the .p file e.g., img1_sep
-    :type p_fname: str
-    :param sample_size: sample size the .p size is based on
-    :type sample_size: int
-    :param auto_l2: if should use automatice 1/sample_size l2 regularisation. Not gauge invariant., defaults to True
-    :type auto_l2: bool, optional
-    :return: argument tuple to pass to subprocess.Popen
-    :rtype: tuple
-    """
-    cm_args = [path_to_exe, "-d",p_dir, "-i",p_fname,"-o", p_fname+"-out", "-b", str(sample_size)]
-    if auto_l2:
-        cm_args.append("-ga")
-    cm_args.append(args)
 
-    return tuple(cm_args)
+    def build_ace_args(path_to_exe:str, p_dir:str ,p_fname:str , sample_size:int, auto_l2=True, *args):
+        """Generate the arguments for the ACE algorithm.
 
-# test that there were no variables deleted -> check that specific line (should never happen since we always add the all 0s and all 1s but better have safeguard)
-def no_deletions(INPUT_dir = "INPUT/data"):
-    """Check if after running ACEtools, if there were variables deleted because they were always 0.
-    This should not happen as it messes up the order later. The subsampler should avoid that this happens.
-    This is a function to perform this sanity check."""
-    for root, dirs, files in os.walk(INPUT_dir):
-        for filename in files:
-            if filename.endswith(".rep"):
-                path = os.path.join(root, filename)
-                with open(path, 'r') as file:
-                    lines = file.readlines()
-                    if len(lines) >= 8 and lines[7].strip() != "":
-                        raise ValueError("Variables were deleted: \n" + lines[7].strip())
+        :param path_to_exe: path to the executable file. e.g., ./bin/ace
+        :type path_to_exe: str
+        :param p_dir: directory of the .p file to do the ace on. e.g., ./INPUT/data/img1
+        :type p_dir: str
+        :param p_fname: filename of the .p file e.g., img1_sep
+        :type p_fname: str
+        :param sample_size: sample size the .p size is based on
+        :type sample_size: int
+        :param auto_l2: if should use automatice 1/sample_size l2 regularisation. Not gauge invariant., defaults to True
+        :type auto_l2: bool, optional
+        :return: argument tuple to pass to subprocess.Popen
+        :rtype: tuple
+        """
+        cm_args = [path_to_exe, "-d",p_dir, "-i",p_fname,"-o", p_fname+"-out", "-b", str(sample_size)]
+        if auto_l2:
+            cm_args.append("-ga")
+        cm_args.append(args)
 
+        return tuple(cm_args)
+
+    def build_qls_args(path_to_exe:str, p_dir:str, p_fname:str, sample_size:int, auto_l2=True, *args):
+        """Generate the arguments for the MC algorithm.
+
+        :param path_to_exe: path to the executable file. e.g., ./bin/ace
+        :type path_to_exe: str
+        :param p_dir: directory of the .p file to do the ace on. e.g., ./INPUT/data/img1
+        :type p_dir: str
+        :param p_fname: filename of the .p file e.g., img1_sep
+        :type p_fname: str
+        :param sample_size: sample size the .p size is based on
+        :type sample_size: int
+        :param auto_l2: if should use automatice 1/sample_size l2 regularisation. Not gauge invariant., defaults to True
+        :type auto_l2: bool, optional
+        :return: argument tuple to pass to subprocess.Popen
+        :rtype: tuple
+        """
+     
+        cm_args = [path_to_exe, "-d", p_dir, "-c", p_fname,"-w",p_fname,"-i",p_fname+"-out", "-o",p_fname+"-out-fit","-b",str(sample_size)]
+        if auto_l2:
+            cm_args.append("-ga")
+        cm_args.append(args)
+        return tuple(cm_args)
 
 # call ACE fitting on it with right flags
 # call MC learning QLS algo on it
@@ -96,6 +145,22 @@ def no_deletions(INPUT_dir = "INPUT/data"):
                     
 # make this a class and one main function to do the steps in order
                     # subsample, convert, ACEtools, nodeletions
+
+
+
+
+
+# def convert_to_spaced(INPUT_dir = "INPUT/data"):
+#     """Converts all files in the INPUT folder and its subfolders from binary strings to binary integers with spaces:
+#     e.g., 000111 -> 0 0 0 1 1 1.
+#     """
+#     for root, dirs, files in os.walk(INPUT_dir):
+#         for filename in files:
+#             if filename.endswith(".dat"):
+#                 path = os.path.join(root, filename)
+#                 file = np.genfromtxt(path, dtype=int, delimiter=1)
+#                 np.savetxt(path[:-4] + "_sep" + ".dat", file, fmt="%d", delimiter=" ")
+
 
 if __name__ == "__main__":
     # helpers.subsample_data_ace(10, all_data_path="./INPUT_all/data", input_data_path="./INPUT/data")
