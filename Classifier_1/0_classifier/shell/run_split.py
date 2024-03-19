a544e1a1b3df12776cda8a4aab656682e08336aa
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-03-19 14:57:32 +0100

    plot subsampling for half works now

--- Classifier_1/0_classifier/shell/run_split.py
+++ Classifier_1/0_classifier/shell/run_split.py
@@ -6,27 +6,26 @@ import src.paper_utils as utils
 import argparse
 
 
+
 def main(sample_s,split_letter):
-    # full sample 6315: train + test
-    n_categories = 10
-    n_variables = 121
-    all_data_path="../INPUT_all/data/combined_split_{}".format(split_letter)
-    result_samples_dir="../OUTPUT/sample_sizes_split_{}".format(split_letter)
-    data_path="../INPUT/data"
-    mcm_filename_format = "train-images-unlabeled-{}_bootstrap_comms.dat"
-    data_filename_format = "full-images-unlabeled-{}.dat"
-    communities_path = "../OUTPUT/comms/"
-    seed = 42 # always same seed
+      
 
-    clf_args = [n_categories, n_variables, mcm_filename_format, data_filename_format, data_path,
-                communities_path]  # data path here for subsampling
+  n_categories = 10 
+  n_variables = 121  
+  mcm_filename_format = "half-images-unlabeled-{}_bootstrap_comms.dat"
+  data_filename_format = "half-images-unlabeled-{}.dat"
+  data_path = "../INPUT/data/"
+  communities_path = "../OUTPUT/comms/"
 
-    for s in range(50):
+  clf_args = [n_categories, n_variables, mcm_filename_format, data_filename_format, data_path, communities_path] 
+  adp = "../INPUT_all/data/combined_split_{}".format(split_letter)
+  rssd = "../OUTPUT/sample_sizes_split_{}".format(split_letter)
+  for i in range(3):
+      utils.evaluate_subsample(sample_s, clf_args,all_data_path=adp, result_sample_sizes_dir=rssd, fname_start="half-", seed=42)
 
-        utils.evaluate_subsample(sample_s, clf_args, all_data_path=all_data_path,
-                                 result_sample_sizes_dir=result_samples_dir, fname_start="half-", seed=seed,
-                                 input_data_path=data_path)
-
+##########3
+  # full sample 6315: train + test
+  
 
 def parse_arguments():
     """Parses command-line arguments and returns a dictionary containing them.
@@ -34,15 +33,15 @@ def parse_arguments():
   Returns:
     A dictionary containing parsed arguments with keys matching the flag names.
   """
-    parser = argparse.ArgumentParser(description="Your script description")
+    parser = argparse.ArgumentParser(description="run split programme")
     parser.add_argument("--sample_s", type=int, required=True,
                         help="Sample size.")
-    parser.add_argument("--split_letter", type=str, required=True,help="A or B")
+    parser.add_argument("--split_letter", type=str, required=True, help="A or B")
 
     return parser.parse_args()
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    main(args.sample_s, args.split_letter)
-    # python3 run_fullsample.py --sample_s 10
+    # args = parse_arguments()
+    # main(args.sample_s, args.split_letter)
+  main(1335,"A")
--- Classifier_1/0_classifier/shell/run_split.sh
+++ Classifier_1/0_classifier/shell/run_split.sh
@@ -8,19 +8,24 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=paul@hosek.de
 
-module load 2022
-module load Miniconda3/4.12.0
-source /sw/arch/RHEL8/EB_production/2022/software/Miniconda3/4.12.0/etc/profile.d/conda.sh
-conda deactivate
-conda activate mcm
+# module load 2022
+# module load Miniconda3/4.12.0
+# source /sw/arch/RHEL8/EB_production/2022/software/Miniconda3/4.12.0/etc/profile.d/conda.sh
+# conda deactivate
+# conda activate mcm
 
 letters=("A" "B")
 
-## Loop through the sample sizes
-for size in 100 500 1000 2000 3000; do
-#    # Loop through the letters
-    for letter in "${letters[@]}"; do
-#        # Call sbatch and run run_split.sh with the current sample size and letter
-      python3 run_split.py --sample_s  $size --split_letter "$letter"
-    done
-done
+size=21
+letter=${letters[0]}
+python3 run_split.py --sample_s  $size --split_letter "$letter"
+
+# ## Loop through the sample sizes
+# for size in 100 500 1000 2000 3000; do
+
+# #    # Loop through the letters
+#     for letter in "${letters[@]}"; do
+# #        # Call sbatch and run run_split.sh with the current sample size and letter
+#       python3 run_split.py --sample_s  $size --split_letter "$letter"
+#     done
+# done
