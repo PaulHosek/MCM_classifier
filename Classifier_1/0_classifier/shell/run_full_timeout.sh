a3e819959ab5192891ed23eaab0726e9d1027fd6
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-04-05 13:46:29 +0200

    cleaning up plots. prepare for full sample run

---
+++ Classifier_1/0_classifier/shell/run_full_timeout.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Number of iterations
+
+iterations=51  
+
+timeout=360
+
+
+for size in 100 1000 2000 3000 4000 5000 6315; do
+  for (( i=1; i <= iterations; i++ )); do  # Removed $ from iterations for consistency
+    
+    # Run the Python script with timeout in the background
+    timeout -s SIGTERM $timeout python3 run_fullsample.py --sample_s $size 
+    python_pid=$!
+
+    # Wait for the Python script to finish or timeout
+    wait $python_pid
+
+    # Check the exit status and kill process if necessary
+    if [[ $? -ne 0 ]]; then
+      echo "Python script timed out or crashed. Killing process..."
+      kill -SIGTERM $python_pid
+    fi
+
+    # Add a short delay between iterations (optional)
+    sleep 5
+  done
+done
+
+
--- Classifier_1/0_classifier/shell/run_fullsample.py
+++ Classifier_1/0_classifier/shell/run_fullsample.py
@@ -8,8 +8,8 @@ import argparse
 
 
 def main(sample_s):
-      
-
+    
+  seed = 1
   n_categories = 10 
   n_variables = 121  
   mcm_filename_format = "full-images-unlabeled-{}_bootstrap_comms.dat"
@@ -20,7 +20,7 @@ def main(sample_s):
   clf_args = [n_categories, n_variables, mcm_filename_format, data_filename_format, data_path, communities_path] 
   adp = "../INPUT_all/data/combined_data"
   rssd = "../OUTPUT/sample_sizes_fromfull"
-  utils.evaluate_subsample(sample_s, clf_args,all_data_path=adp, result_sample_sizes_dir=rssd, fname_start="half-", seed=None)
+  utils.evaluate_subsample(sample_s, clf_args,all_data_path=adp, result_sample_sizes_dir=rssd, fname_start="full-", seed=seed, seed_plus=True)
 
 ##########3
   # full sample 6315: train + test
@@ -32,14 +32,18 @@ def parse_arguments():
   Returns:
     A dictionary containing parsed arguments with keys matching the flag names.
   """
-    parser = argparse.ArgumentParser(description="run split programme")
+    parser = argparse.ArgumentParser(description="run full sampling regime")
     parser.add_argument("--sample_s", type=int, required=True,
                         help="Sample size.")
-    parser.add_argument("--split_letter", type=str, required=True, help="A or B")
 
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.sample_s, args.split_letter)
+    main(args.sample_s)
+
+
+
+
+
