abb52f8062484bf1638ada0926ae1b67af64f4d6
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-04-23 14:24:45 +0200

    modelfitting plots results

--- Classifier_1/1_pairwise/toy_systems/gen/small_sys.py
+++ Classifier_1/1_pairwise/toy_systems/gen/small_sys.py
@@ -2,20 +2,22 @@ import numpy as np
 
 
 seed = 1
-def gen_data():
+def gen_data(spins):
+
     rng = np.random.default_rng(seed)
     # data = rng.integers(2, size= (10000,8))
-    data = np.ones((5000,8))
+    data = rng.integers(0,2,(10000,spins))
     data[:2500,[1,2,3]] = 0
 
     data[:300, 2] = data[:300,3]
     data[:3000, 1] = data[:3000, 2]
 
-    data[data==0] = 2
+    # data[data==0] = 2
     return data
 
 
 if __name__ == "__main__":
-    res = gen_data()
+    spins = 14
+    res = gen_data(spins)
 #   pij = np.einsum("i,ij,ik->jk")
-    np.savetxt("8spin_12.dat", res,fmt="%d", delimiter=" ")
+    np.savetxt(f"{spins}spin.dat", res,fmt="%d", delimiter=" ")
