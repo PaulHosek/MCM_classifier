d25fbc771163260008bdd3c79096035bae18f733
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-04-23 10:43:34 +0200

    got energy for all states

--- Classifier_1/1_pairwise/toy_systems/experiments/pairwise_evaluator.py
+++ Classifier_1/1_pairwise/toy_systems/experiments/pairwise_evaluator.py
@@ -42,7 +42,6 @@ class Pairwise_evaluator():
         if all_param.shape[0] != N + N*(N-1)/2:
             raise ValueError(f"Nr of total paramters, shape ({all_param.shape}), dim0 does not match expected {N + N(N-1)/2} samples based on {N} spins.")
 
-
     def calc_energy(self, state): # state assumed to be in convention -1 1
         h = self.__calc_fields(self.fields, state)
         j = self.__calc_couplings(self.couplings,state)
@@ -63,14 +62,39 @@ class Pairwise_evaluator():
         return np.sum(couplings*((state[:,None]*state)[~np.tri(len(state),dtype=bool)]))
     
     # numba this as well
-    def partiton_function():
+
+    @staticmethod
+    def unpackbits2d(x, num_bits):
+        if np.issubdtype(x.dtype, np.floating):
+            raise ValueError("numpy data type needs to be int-like")
+        xshape = list(x.shape)
+        x = x.reshape([-1, 1])
+        mask = 2**np.arange(num_bits, dtype=x.dtype).reshape([1, num_bits])
+        return (x & mask).astype(bool).astype(int).reshape(xshape + [num_bits])[:,::-1]
+
+    def partiton_function(self):
         # generate the value for Z
         # this is the most expensive, only do once -> class
         # calcualte portion we observed and assign mass there, all other ones we do not need to generate since all the same
             # assign their value proportionally to their part of 2^N states
 
         # 1. Calculate energy for every observed state
-        pass
+
+        # 1. generate every possible state
+        assert self.nspins <= 15, "> 15 spins. Avoid calculating Z."
+        all_states = self.unpackbits2d(np.arange(2**self.nspins), self.nspins)
+        # 2. calculate energy or each state
+        all_E = np.apply_along_axis(self.calc_energy,1,all_states)
+
+
+        return res
+
+
+        # 3. take exp of - energy of every state and sum the results up over all states
+
+        # get Z out
+
+
 
 
     def predict(state):
@@ -81,3 +105,5 @@ if __name__ == "__main__":
     spin4_path = "../output_small/4spin/4spin_sep-output-out.j"
     mod = Pairwise_evaluator(spin4_path,4)
     mod.load_ising_paramters()
+    Z = mod.partiton_function()
+    print(Z)
