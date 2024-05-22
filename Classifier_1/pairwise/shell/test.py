a4d6f1fb19b2b4f4faa921647e87f3f4686e2860
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-05-22 14:57:23 +0200

    confusion matrix done

---
+++ Classifier_1/pairwise/shell/test.py
@@ -0,0 +1,7 @@
+
+
+
+if __name__ == "__main__":
+    mystr = ['../ace_utils/ace', '-d', '../data/OUTPUT_mod/data/full_sample_sizes/15/1/full-images-unlabeled-0', '-i', 'full-images-unlabeled-0_sep-output', '-o', 'full-images-unlabeled-0_sep-output-out', '-b', '15', '-ga']
+    print(" ".join(mystr))
+
