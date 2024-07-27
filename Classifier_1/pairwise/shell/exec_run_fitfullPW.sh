15432da95617f159ad2910880bc8264687b7e55a
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-07-27 14:18:07 +0200

    panel pix done

--- Classifier_1/pairwise/shell/exec_run_fitfullPW.sh
+++ Classifier_1/pairwise/shell/exec_run_fitfullPW.sh
@@ -6,13 +6,21 @@
 # do
 
 #for size in 10 100 500 $(seq 1000 1000 6000) 6315 ; do 
-for size in  500;do 
-  echo $size
-   for digit in {0..1} 
-   do
-    python3 run_fitfullPW.py --sample_s $size --digit $digit --method "rise" --trainfull "train"
-   done
-done
+
+# for 6315 in  500;do 
+#   echo $size
+#    for digit in 0,1,3,5; do 
+#    do
+#     python3 run_fitfullPW.py --sample_s $size --digit $digit --method "ace" --trainfull "full"
+#    done
+# done
+
 
 # done
 
+for size in 6315; do 
+  echo $size
+  for digit in 0 1 3 5; do 
+    timeout 3600 python3 run_fitfullPW.py --sample_s $size --digit $digit --method "ace" --trainfull "full"
+  done
+done
