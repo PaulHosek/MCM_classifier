92436fdc39dc23100adf4d490cdce2d470df177c
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2024-07-30 10:24:34 +0200

    4h all 0,1,3,5

--- Classifier_1/pairwise/shell/exec_run_fitfullPW.sh
+++ Classifier_1/pairwise/shell/exec_run_fitfullPW.sh
@@ -20,7 +20,7 @@
 
 for size in 6315; do 
   echo $size
-  for digit in 0; do 
+  for digit in 1 3 5; do 
     timeout 14400 python3 run_fitfullPW.py --sample_s $size --digit $digit --method "ace" --trainfull "full"
   done
 done
